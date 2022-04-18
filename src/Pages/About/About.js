4d8389dd94695957e73da01a542b9c27be68df5c
shuvo-99 <anontoshuvo99@gmail.com>
2022-04-18 22:25:34 +0600

    aboout me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
-      <h2>This is about</h2>
+      <Container>
+        <Row>
+          <Col>
+            <p>Name: Md. Anonto Shuvo</p>
+            Goal: I am currently a CSE student and that's why my goal is to
+            become a software developer. I am learning web development beacuse
+            it is also an important part for a developer.
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </div>
   );
 };
