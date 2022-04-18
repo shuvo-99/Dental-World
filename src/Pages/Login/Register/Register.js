76ece8349ebcec8db47b0c0005b0f537424d7296
shuvo-99 <anontoshuvo99@gmail.com>
2022-04-18 13:57:36 +0600

    login & register layout

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -29,24 +29,23 @@ const Register = () => {
   //   console.log("user", user);
   // }
 
-  // const handleRegister = async (event) => {
-  //   event.preventDefault();
-  //   const name = event.target.name.value;
-  //   const email = event.target.email.value;
-  //   const password = event.target.password.value;
-  //   // const agree = event.target.terms.checked;
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    //   // const agree = event.target.terms.checked;
 
-  //   await createUserWithEmailAndPassword(email, password);
-  //   await updateProfile({ displayName: name });
-  //   console.log("Updated profile");
-  //   navigate("/home");
-  // };
+    //   await createUserWithEmailAndPassword(email, password);
+    //   await updateProfile({ displayName: name });
+    //   console.log("Updated profile");
+    //   navigate("/home");
+  };
 
   return (
     <div className="register-form">
       <h2 style={{ textAlign: "center" }}>Please Register</h2>
-      {/* <form onSubmit={handleRegister}> */}
-      <form>
+      <form onSubmit={handleRegister}>
         <input type="text" name="name" id="" placeholder="Your Name" />
 
         <input
@@ -71,27 +70,27 @@ const Register = () => {
           id="terms"
         /> */}
         {/* <label className={agree ? 'ps-2': 'ps-2 text-danger'} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
-        <label className={`ps-2 ${agree ? "" : "text-danger"}`} htmlFor="terms">
+        {/* <label className={`ps-2 ${agree ? "" : "text-danger"}`} htmlFor="terms">
           Accept Genius Car Terms and Conditions
-        </label>
+        </label> */}
         <input
-          disabled={!agree}
+          // disabled={!agree}
           className="w-50 mx-auto btn btn-primary mt-2"
           type="submit"
           value="Register"
         />
       </form>
-      <p>
+      <p className="text-center">
         Already have an account?{" "}
         <Link
           to="/login"
-          className="text-primary pe-auto text-decoration-none"
+          className="text-danger pe-auto text-decoration-none"
           onClick={navigateLogin}
         >
           Please Login
         </Link>{" "}
       </p>
-      <SocialLogin></SocialLogin>
+      {/* <SocialLogin></SocialLogin> */}
     </div>
   );
 };
