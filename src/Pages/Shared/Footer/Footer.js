6e082763f0cd7dbe8fb05eea4a75dd103e5bd4ba
shuvo-99 <anontoshuvo99@gmail.com>
2022-04-18 23:19:07 +0600

    finishing

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="text-center mt-5">
       <p>
-        <small>copyright @ {year} </small>
+        <small>copyright @ Dental World || All Rights Reserved {year} </small>
       </p>
     </footer>
   );
